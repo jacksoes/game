ea4bd90996c8c50113c3bc9252c32871267bdb9b
jack soesman <jsoesman1@yahoo.com>
2024-03-09 17:44:19 -0500

    organize and make class based

--- app.js
+++ app.js
@@ -1,61 +1,37 @@
-let position = {
-    first: 1300,
-    second: 1500, 
-    third: 1200, 
-    fourth: 1400, 
-    fifth: 1600 
-}
-
-let enemySpawnY = 1300;
-
-let mouseXControl;
-let mouseYControl;
-
-let cuts1 = 0;
-
 function setup() {
     createCanvas(800, 1200);
-  }
+
+    squareOne = new enemySquare(400, 800, 100)
+
+    squareTwo = new enemySquare(200, 500, 150)
+}
   
 function draw() {
     background('rgb(119,255,119)');
     loadUser()
-    loadGUI()
+    loadGUI();
     
     // spawn enemy object and make move.
-    const square = new enemySquare(interface_enemy.size)
-    square.spawn(position.first, cuts1);
-    position.first -= 1
-        if (position.first < -50){
-            position.first = enemySpawnY ;
-        }
+    squareOne.spawn()
+    squareOne.move()
 
-    
-    const square2 = new enemySquare(interface_enemy.size)
-    square2.spawn(position.second, 0)
-    position.second -= 1
-        if (position.second < -50){
-            position.second = enemySpawnY ;
-        }
-
-
-     
-        
-
+    squareTwo.spawn()
+    squareTwo.move()
 }
 
-function mousePressed() {
-    // if mouse presses first square
-    if (dist(mouseX, mouseY, 400, position.first + 60) <= 75){
-        if (cuts1 == 4)
-            cuts1 = 0;
-        else
-            cuts1 += 1;
+function mousePressed() {    // if mouse presses square, cut.
+
+    if (dist(mouseX, mouseY, squareOne.positionX, squareOne.positionY) <= 100) {
+    squareOne.incrementCuts()
+    console.log(`position x: ${squareOne.positionX}, positionY: ${squareOne.positionY}\n mousX: ${mouseX}, mouseY: ${mouseY}`)
     }
-    // make user an object
-    // if user coordiates = square => do action
-   
-  }
+    if (dist(mouseX, mouseY, squareTwo.positionX, squareTwo.positionY) <= 100){
+        squareTwo.incrementCuts()
+    }
+}
+
+
+
 
 function loadUser(){
     strokeWeight(10)
@@ -75,40 +51,51 @@ function loadGUI() {
     fill(0, 0); // lets squares be overlapped by cursor.
 }
 
-
-
-const interface_enemy = {
-    size_small: 50,
-    size: 100,
-    size_large: 150,
-} 
-
 class enemySquare {
-    constructor(size)
-    {
-        this.size = size;
-        this.position = 800;   
+    constructor(positionX, positionY, size)
+    {   
+        this.positionX = positionX
+        this.positionY = positionY
+        this.size = size
+        this.cuts = 0
+
     }
-    spawn(position, cuts){
-        switch (cuts){
+
+  
+
+    
+    spawn(){
+        
+        switch (this.cuts){
             case 1:
-                square(400, position, this.size, 50, 0, 0, 0)
+                square(this.positionX, this.positionY, this.size, 50, 0, 0, 0)
                 break;
             case 2:
-                square(400, position, this.size, 50, 50, 0, 0)
+                square(this.positionX, this.positionY, this.size, 50, 50, 0, 0)
                 break;
             case 3:
-                square(400, position, this.size, 50, 50, 50, 0)
+                square(this.positionX, this.positionY, this.size, 50, 50, 50, 0)
                 break;
             case 4:
-                square(400, position, this.size, 50, 50, 50, 50)
+                square(this.positionX, this.positionY, this.size, 50, 50, 50, 50)
                 break;
-            default:    
-                square(400, position, this.size)
-                break;
+            default:   
+                square(this.positionX, this.positionY, this.size)
+                //break;
         }
+    }
+
+    move(){
+        this.positionY -= 1
+    }
         
+    incrementCuts(){
+        if (this.cuts >= 4)
+            this.cuts = 0
+        else
+            this.cuts += 1;
     }
 
 }
-    
+
+
